25d8aa678675955c4c6437fc82d7ec86a6140f30
mc-tangb <511598230@qq.com>
2017-05-17 20:11:27 +0800

    CM

---
+++ src/page/home.vue
@@ -0,0 +1,5 @@
+<template>
+	<div>
+		<h1>home.html</h1>
+	</div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 
 Vue.use(Router)
 
@@ -8,8 +7,14 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'home',
+      component: r => require.ensure([], r(require('@/page/home.vue')))
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: r => require.ensure([], r(require('@/page/login.vue')))
     }
   ]
 })
+
