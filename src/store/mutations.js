e3713988f24128b8a0a20a88a112ca4ee5c24b05
mc-tangb <511598230@qq.com>
2017-06-05 23:32:50 +0800

    改进联动

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,17 +1,30 @@
 import { FILTER_VALUE, SET_DATA } from './mutation-types'
-import { isObjectEquals } from '@/utils/common'
+import common from '@/utils/common'
 export default {
 	[FILTER_VALUE](state, filter) {
-		let {filters, data} = state;
-		filters.push([filter['key'], filter['name']]);
-		data['data'] = data['data'].filter(function(obj){
-			return obj[filter['key']] == filter['name'];
-		})
-		console.log(state.data);
+		let {filters, data, _cache} = state;
+		let index = -1;
+		for(let i=0; i<filters.length; i++) {
+			if(filters[i][0] == filter['key'] && filters[i][1] == filter['name']){
+				index = i;
+				break;
+			}
+		}
+		if(index == -1) {
+			filters.push([filter['key'], filter['name']]);
+		}else {
+			filters.splice(index, 1);
+		}
+		data['data'] = common.deepCopy(_cache['data']);
+		for(let i=0; i<filters.length; i++) {
+			data['data'] = data['data'].filter(function(obj){
+				return obj[filters[i][0]] == filters[i][1];
+			});
+		}
 	},
 
 	[SET_DATA](state, data) {
-		state._cache = data;
+		state._cache = common.deepCopy(data);
 		state.data = data;
 	}
 
