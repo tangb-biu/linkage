34eafe8bc999f0e371103e5997d3e6a4035de999
mc-tangb <511598230@qq.com>
2017-06-11 21:40:12 +0800

    添加过滤显示条

---
+++ src/components/chooseBar.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="btn-group" role="group">
+		<button type="button" class="btn btn-default" v-for="item in filters">{{ item }}</button>
+	</div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+export default {
+	data() {
+		return {
+
+		}
+	},
+
+	methods: {
+
+	},
+
+	computed: {
+		...mapState([
+			'filters'
+		])
+	}
+}
+</script>
+
+<style lang="less" scoped>
+	
+</style>
